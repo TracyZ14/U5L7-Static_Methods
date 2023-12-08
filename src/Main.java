fb32bfab8faba3097a24aa4930fd7798b2bcb38f
Tracy Zhou <TracyZ14@nycstudents.net>
2023-12-08 00:20:48 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Box box = new Box(15, 12, 17);
+
+        double len = 6.5;
+        double feetInches = box.feetToInches(len);
+        double meterInches = box.metersToInches(len);
+        double volume = box.volume();
+
+        System.out.println(feetInches);
+        System.out.println(meterInches);
+        System.out.println(volume);
+    }
+}
